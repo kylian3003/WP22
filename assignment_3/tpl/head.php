5520bf88a02fff08a85130c081db4654b4ea20b0
kylian3003 <32676953+kylian3003@users.noreply.github.com>
2022-06-03 13:05:07 +0200

    added tpl files

---
+++ assignment_3/tpl/head.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+          <meta charset="utf-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+          <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
+                    integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn"
+                    crossorigin="anonymous">
+          <title>assignment_3</title>
+</head>
+
+<!--  -->
+<div class="container">
+          <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <a class="navbar-brand" href="#">Navbar</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse"
+                              data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                              aria-expanded="false" aria-label="Toggle navigation">
+                              <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                              <ul class="navbar-nav ml-auto">
+                                        <li class="nav-item active">
+                                                  <a class="nav-link" href="index.php">Home</a>
+                                        </li>
+                                        <li class="nav-item active">
+                                                  <a class="nav-link" href="news_add.php">Add news</a>
+                                        </li>
+                                        <li class="nav-item active">
+                                                  <a class="nav-link" href="leap_year.php">Leap years</a>
+                                        </li>
+                                        <li class="nav-item active">
+                                                  <a class="nav-link" href="simple_form.php">Simple form</a>
+                                        </li>
+                              </ul>
+
+                    </div>
+          </nav>
+</div>
